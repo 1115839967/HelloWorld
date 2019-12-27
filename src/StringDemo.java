4b125f95f0ca274438715e6ee21baae669139167
xutiancheng <xutiancheng@qgutech.com>
2019-12-27 13:23:58 +0800

    字符串切割、转换成list

--- src/StringDemo.java
+++ src/StringDemo.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @program: HelloWorld
  * @description:
@@ -9,5 +12,12 @@ public class StringDemo {
 
         String code = "90006006";
         System.out.println(code.substring(3));
+
+        String idStr = "123,456,789";
+        String[] split = idStr.split(",");
+        List<String> idList = Arrays.asList(split);
+        idList.forEach(id -> {
+            System.out.println(id);
+        });
     }
 }
