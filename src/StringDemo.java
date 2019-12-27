ea832b3f0ef04809af21506d697991bf57386028
xutiancheng <xutiancheng@qgutech.com>
2019-12-27 13:24:26 +0800

    去除注释

--- src/StringDemo.java
+++ src/StringDemo.java
@@ -1,12 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * @program: HelloWorld
- * @description:
- * @author: liu yan
- * @create: 2019-08-28 11:01
- */
 public class StringDemo {
     public static void main(String[] args) {
 
